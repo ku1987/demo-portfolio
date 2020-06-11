826fa0a3cff0b2975f5ba4db23522cb74bcdba6d
ku1987 <kei.usami1987@gmail.com>
2020-06-12 00:29:51 +0900

    Fix CI

--- server/app.js
+++ server/app.js
@@ -1,5 +1,9 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const compression = require('compression');
+const koaConnect = require('koa-connect');
+const koaBunyanLogger = require('koa-bunyan-logger');
+
 // const serve = require('koa-static');
 const json = require('koa-json');
 // const path = require('path');
@@ -13,7 +17,14 @@ const config = require('./config');
 const { port } = config;
 const app = new Koa();
 
-// app.use(bodyParser);
+app.use(koaBunyanLogger());
+
+app.use((ctx, next) => {
+  ctx.log.info('Got a request from %s for %s', ctx.request.ip, ctx.path);
+  return next();
+});
+
+app.use(koaConnect(compression()));
 app.use(json());
 app.use(cors());
 app.use(bodyParser());
