fb6ca35719a1ad6c92d6d443288e192a859b44c7
ku1987 <kei.usami1987@gmail.com>
2020-06-15 21:17:50 +0900

    Change deploy process

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,6 @@ git pull origin master
 cd client
 npm run build
 pm2 delete all
-pm2 --name HelloWorld start npm -- start
+pm2 --name client start npm -- start
+cd ../server
+pm2 --name server start npm -- dev
