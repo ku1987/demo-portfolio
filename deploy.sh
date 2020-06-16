c7842cbb159c697ab4cae0e9929a333ca67aaaaa
ku1987 <kei.usami1987@gmail.com>
2020-06-16 21:19:38 +0900

    Fix deploy process

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ git pull origin master
 cd client
 npm run build
 pm2 delete all
-pm2 --name client restart npm -- start
+pm2 restart client
 cd ../server
-pm2 --name server restart npm -- start
+pm2 restart server
