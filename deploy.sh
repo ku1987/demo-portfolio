81506e550e2bf02ac359169b068e7d6d15dc332c
ku1987 <kei.usami1987@gmail.com>
2020-07-19 11:37:02 +0900

    Fix deploy shell

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ git pull origin master
 npm ci --prefix client
 npm run build --prefix client
 pm2 stop client
-pm2 start pm2-client
+pm2 start pm2-client.yml
 npm ci --prefix client
 pm2 stop server
-pm2 start pm2-server
+pm2 start pm2-server.yml
