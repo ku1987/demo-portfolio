efc80d90c708222eb4cdbcb4f7ac69a1e95fc2be
ku1987 <kei.usami1987@gmail.com>
2020-11-17 22:04:25 +0900

    Use yarn

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-npm ci --prefix client
+yarn --prefix client
 pm2 stop client
-npm run build --prefix client
+yarn build --prefix client
 pm2 start pm2-client.yml
-npm ci --prefix server
+yarn --prefix server
 pm2 stop server
 pm2 start pm2-server.yml
